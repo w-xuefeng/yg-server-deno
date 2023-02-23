042299c8fd2b7b82bf422400acfd15c164835628
XueFLY <xuefeng@kcalb.wang>
2023-02-23 10:31:16 +0000

    feat: add denodb

--- shared/deps.ts
+++ shared/deps.ts
@@ -2,4 +2,5 @@ export * from "https://deno.land/x/oak@v11.1.0/mod.ts";
 export * from "https://cdn.jsdelivr.net/gh/w-xuefeng/denokit@latest/mod.ts";
 export * as colors from "https://deno.land/std@0.53.0/fmt/colors.ts";
 export { default as logger } from "https://deno.land/x/oak_logger@1.0.0/mod.ts";
+export { Database, MySQLConnector } from "https://deno.land/x/denodb@v1.2.0/mod.ts";
 export { default as dotenv } from "./dotenv.ts";
