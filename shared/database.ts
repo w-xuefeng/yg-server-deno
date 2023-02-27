b585ac70d014399ba361c8bc7cbd3377bd934728
XueFLY <xuefeng@kcalb.wang>
2023-02-27 08:18:32 +0000

    fix: fixed DenoDB deps

--- shared/database.ts
+++ shared/database.ts
@@ -1,7 +1,7 @@
-import { Database, MySQLConnector, MySQLOptions } from "./deps.ts";
+import { DenoDB } from "./deps.ts";
 
-export function useMySQL(options: MySQLOptions) {
-  const connector = new MySQLConnector(options);
-  const db = new Database(connector);
+export function useMySQL(options: DenoDB.MySQLOptions) {
+  const connector = new DenoDB.MySQLConnector(options);
+  const db = new DenoDB.Database(connector);
   return db;
 }
