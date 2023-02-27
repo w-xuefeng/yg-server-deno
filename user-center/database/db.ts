88f11598655ab19f20157681d93a5839a8db5b33
XueFLY <xuefeng@kcalb.wang>
2023-02-27 10:26:17 +0000

    chiore: modify env file

--- user-center/database/db.ts
+++ user-center/database/db.ts
@@ -1,4 +1,10 @@
 import { useMySQL } from "../../shared/database.ts";
 import config from "../configs/const.ts";
+import Role from "../models/role.ts";
+import User from "../models/user.ts";
 
 export const db = useMySQL(config.database);
+
+export const linkModel = () => {
+  db.link([Role, User]);
+};
