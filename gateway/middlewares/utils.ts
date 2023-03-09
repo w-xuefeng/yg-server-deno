9f40b876f14e9e0024ee2cd36f2dbdcdf7844818
XueFLY <xuefeng@kcalb.wang>
2023-03-10 01:33:29 +0800

    feat: add sLog

--- gateway/middlewares/utils.ts
+++ gateway/middlewares/utils.ts
@@ -1,14 +1,23 @@
 import { colors, type Context, format } from "../../shared/deps.ts";
+import { sLog } from "../../shared/write-log.ts";
 
 export function logTraceId(middlewareName: string, ctx: Context) {
-  const middleware = colors.gray(`[Gateway ${middlewareName}]`);
-  const method = colors.blue(ctx.request.method);
-  const href = ctx.request.url.href;
+  const pureMiddleware = `[Gateway ${middlewareName}]`;
+  const pureMethod = ctx.request.method;
+  const pureHref = ctx.request.url.href;
+  const pureContent = `${("traceId:")} ${ctx.state.traceId}`;
+  const pureTime = format(new Date(Date.now()), "MM-dd-yyyy HH:mm:ss.SSS");
+
+  const middleware = colors.gray(pureMiddleware);
+  const method = colors.blue(pureMethod);
+  const href = pureHref;
   const content = `${colors.gray("traceId:")} ${
     colors.italic(ctx.state.traceId)
   }`;
-  const time = colors.gray(
-    format(new Date(Date.now()), "MM-dd-yyyy HH:mm:ss.SSS"),
-  );
+  const time = colors.gray(pureTime);
+
   console.log(`${middleware} ${method} ${href} ${content} ${time}`);
+  sLog(
+    `${pureMiddleware} ${pureMethod} ${pureHref} ${pureContent} ${pureTime}`,
+  );
 }
